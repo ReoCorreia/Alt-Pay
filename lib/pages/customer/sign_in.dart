5926a3941a58ec62ee27f9c95af9b8224ec26a28
Arshan Bhanage <bhanagearshan@gmail.com>
2024-04-04 12:41:13 +0530

    added animations to sign-in

--- lib/pages/customer/sign_in.dart
+++ lib/pages/customer/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter_application_1/pages/customer/dashboard.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 import 'package:flutter_application_1/styles/button.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,17 +17,57 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
 
   String _selectedCountryCode = "+91"; // Default country code
+  bool incorrectPhone = false;
+  bool incorrectPassword = false;
   
   final TextEditingController _phoneNumberController = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
   void _submit(BuildContext context){
-    print('Phone: ${_phoneNumberController.text}');
-    print('Password: ${_password.text}');
-    if(_phoneNumberController.text.isEmpty || _password.text.isEmpty){
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please fill all  fields')));
+
+    if(_phoneNumberController.text.isEmpty){
+      setState(() {
+        incorrectPhone = true;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            textAlign: TextAlign.center,
+            'Phone Incorrect',
+            style: GoogleFonts.getFont(
+              'Lato',
+              fontSize: 18,
+              color: textWhite,
+              fontWeight: FontWeight.bold,
+              letterSpacing: .7,
+            ),            
+          ).animate(target: incorrectPhone? 1 : 0).shakeX(hz: 14, curve: Curves.easeInOutCubic), 
+          backgroundColor: themeBtnOrange
+        ),
+      );     
       return;
-    }
+    }else if(_password.text.isEmpty){
+      setState(() {
+        incorrectPassword = true;
+      });      
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            textAlign: TextAlign.center,
+            'Password Incorrect',
+            style: GoogleFonts.getFont(
+              'Lato',
+              fontSize: 18,
+              color: textWhite,
+              fontWeight: FontWeight.bold,
+              letterSpacing: .7,
+            ),            
+          ).animate(target: incorrectPassword? 1 : 0).shakeX(hz: 14, curve: Curves.easeInOutCubic), 
+          backgroundColor: themeBtnOrange
+        ),
+      );
+      return;
+    }    
     Navigator.push(context, MaterialPageRoute(builder: (context) => const Dashboard()));
   }
 
@@ -34,18 +75,30 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: themeBtnOrange,
-        title: const Text('Sign In'),
+          title: Text(
+            textAlign: TextAlign.center,
+            'Sign In',
+            style: GoogleFonts.getFont(
+              'Lato',
+              fontSize: 18,
+              color: textWhite,
+              fontWeight: FontWeight.bold,
+              letterSpacing: .7,
+            ),            
+          )
       ),      
       body: Padding(
         padding: const EdgeInsets.all(25.0),
         child: ListView(
           children: <Widget>[
+            // target: incorrectInput? 1 : 0 can be used inside animate() to conditionally animate.
             Image.asset('lib/images/login.png', width: 300, height: 300),
             // const SizedBox(height: 20.0),
             Container(
               decoration: BoxDecoration(
-                border: Border.all(color: themeBtnOrange), // Define border color and width
+                border: Border.all(color: incorrectPhone? Colors.red : themeBtnOrange), // Define border color and width
                 borderRadius: BorderRadius.circular(8.0), // Optionally, apply border radius
               ),
               child: Row(
@@ -75,17 +128,19 @@ class _SignInState extends State<SignIn> {
                   ),
                 ],
               ),
-            ),
+            ).animate(target: incorrectPhone? 1 : 0).shakeX(hz: 14, curve: Curves.easeInOutCubic),
             const SizedBox(height: 20.0),
             Row(
               children: [
+                    // effects: [FadeEffect(), ScaleEffect()],
                 Expanded(
                   flex: 2,
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8.0), // Apply border radius
-                      border: Border.all(color: themeBtnOrange), // Define border color and width
+                      border: Border.all(color: incorrectPassword? Colors.red : themeBtnOrange), // Define border color and width
                     ),
+
                     child: TextField(
                       obscureText: true,
                       decoration: const InputDecoration(
@@ -95,7 +150,7 @@ class _SignInState extends State<SignIn> {
                       ),
                       controller: _password,
                     ),
-                  ),
+                  ).animate(target: incorrectPassword? 1 : 0).shakeX(hz: 14, curve: Curves.easeInOutCubic),
                 ),
               ],
             ),
